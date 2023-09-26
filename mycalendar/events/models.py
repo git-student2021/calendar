bf9d211ff5d667a14db218eff5eb7082c9148197
Andrei <student.andrey.2022@gmail.com>
2023-09-26 16:11:51 +0300

    created a model

--- mycalendar/events/models.py
+++ mycalendar/events/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class Events(models.Model):
+    title = models.CharField(max_length=150, verbose_name="Название события")
+    description_event = models.TextField(blank=True, verbose_name="Описание")
+    start_of_the_event = models.DateTimeField(verbose_name="Дата начала")
+    end_event = models.DateTimeField(verbose_name="Дата завершения")
+    need_for_notification = models.BooleanField(default=True, verbose_name="Напомнить")
