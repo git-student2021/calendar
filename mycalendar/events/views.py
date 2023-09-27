c8009e293aef1b652781cee1cec07876f5ddfcaf
Andrei <student.andrey.2022@gmail.com>
2023-09-27 16:56:55 +0300

    deleted test. Updated index.

--- mycalendar/events/views.py
+++ mycalendar/events/views.py
@@ -5,7 +5,4 @@ from .models import Events
 
 def index(request):
     events = Events.objects.all()
-    res = '<h1>Список событий</h1>'
-    for i in events:
-        res += f'<div>\n<p>{i.title}</p>\n<p>{i.description_event}</p>\n</div>\n<hr>'
     return render(request, 'events/index.html', {'events': events, 'title': 'Список событий'})
--- mycalendar/mycalendar/urls.py
+++ mycalendar/mycalendar/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from events.views import index, test
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
