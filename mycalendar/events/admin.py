c08b21c5533a934839d35d7b1b4796fe07888940
Andrei <student.andrey.2022@gmail.com>
2023-09-29 11:32:51 +0300

    added fields in the admin panel

--- mycalendar/events/admin.py
+++ mycalendar/events/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Events
 
-admin.site.register(Events)
+class EventsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'start_of_the_event', 'need_for_notification')
+
+admin.site.register(Events, EventsAdmin)
