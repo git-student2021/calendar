a1296fe961403d7832fe0a545f58ba89794a68a9
Andrei <student.andrey.2022@gmail.com>
2023-09-29 15:23:15 +0300

    updated admin.

--- mycalendar/events/admin.py
+++ mycalendar/events/admin.py
@@ -6,5 +6,7 @@ class EventsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'start_of_the_event', 'need_for_notification')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'description_event')
+    list_editable = ('need_for_notification',)
+    list_filter = ('need_for_notification',)
 
 admin.site.register(Events, EventsAdmin)
