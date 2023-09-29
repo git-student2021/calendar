8696b8d63b25bfaa679f623736e2a150b38f3040
Andrei <student.andrey.2022@gmail.com>
2023-09-29 11:39:56 +0300

    added search to admin panel

--- mycalendar/events/admin.py
+++ mycalendar/events/admin.py
@@ -5,5 +5,6 @@ from .models import Events
 class EventsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'start_of_the_event', 'need_for_notification')
     list_display_links = ('id', 'title')
+    search_fields = ('title', 'description_event')
 
 admin.site.register(Events, EventsAdmin)
