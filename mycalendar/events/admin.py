6978e309de0d52f5cdc05d5a318fb672a181618f
Andrei <student.andrey.2022@gmail.com>
2023-09-29 15:36:07 +0300

    updated admin.

--- mycalendar/events/admin.py
+++ mycalendar/events/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Events
 
 class EventsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'start_of_the_event', 'need_for_notification')
+    list_display = ('id', 'title', 'start_of_the_event', 'end_event', 'need_for_notification')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'description_event')
     list_editable = ('need_for_notification',)
