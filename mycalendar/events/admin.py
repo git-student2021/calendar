91e86df4c3cbad317fbbaf8b074d9821d4bf559d
Andrei <student.andrey.2022@gmail.com>
2023-09-29 11:37:03 +0300

    added links to admin panel

--- mycalendar/events/admin.py
+++ mycalendar/events/admin.py
@@ -4,5 +4,6 @@ from .models import Events
 
 class EventsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'start_of_the_event', 'need_for_notification')
+    list_display_links = ('id', 'title')
 
 admin.site.register(Events, EventsAdmin)
