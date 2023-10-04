3b1c31488ce4324ff1344d1af7b9cd492ec0700f
Andrei <student.andrey.2022@gmail.com>
2023-10-04 15:47:18 +0300

    created model Time_interval. registered model in admin.py

--- mycalendar/events/admin.py
+++ mycalendar/events/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 
-from .models import Events
+from .models import Events, Time_interval
 
 class EventsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'start_of_the_event', 'end_event', 'need_for_notification')
+    list_display = ('id', 'title', 'start_of_the_event', 'end_event', 'need_for_notification', 'interval')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'description_event')
     list_editable = ('need_for_notification',)
     list_filter = ('need_for_notification',)
 
+class Time_intervalAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
 admin.site.register(Events, EventsAdmin)
+admin.site.register(Time_interval, Time_intervalAdmin)
--- mycalendar/events/models.py
+++ mycalendar/events/models.py
@@ -19,6 +19,7 @@ class Events(models.Model):
     start_of_the_event = models.DateTimeField(default=timezone.now, verbose_name="Дата начала")
     end_event = models.DateTimeField(default=one_week_hence, verbose_name="Дата завершения")
     need_for_notification = models.BooleanField(default=True, verbose_name="Напомнить")
+    interval = models.ForeignKey('Time_interval', on_delete=models.PROTECT, null=True, verbose_name="Интервал")
 
     def __str__(self):
         return self.title
@@ -27,3 +28,14 @@ class Events(models.Model):
         verbose_name = 'Событие'
         verbose_name_plural = 'События'
         ordering = ['start_of_the_event']
+
+class Time_interval(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name="Интервал")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Интервал'
+        verbose_name_plural = 'Интервалы'
+        ordering = ['title']
