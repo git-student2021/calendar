0a97e63ecad1b04d94a8cd0e6e2de27941e18993
Andrei <student.andrey.2022@gmail.com>
2023-10-09 16:44:40 +0300

    updated index.html, updated views.py

--- mycalendar/events/views.py
+++ mycalendar/events/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Events
+from .models import Events, Time_interval
 
 def index(request):
     events = Events.objects.all()
-    return render(request, 'events/index.html', {'events': events, 'title': 'Список событий'})
+    intervals = Time_interval.objects.all()
+    context = {
+        'events': events,
+        'title': 'Список событий',
+        'intervals': intervals,
+    }
+    return render(request, 'events/index.html', context=context)
