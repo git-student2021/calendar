8080a9fab053b74faea27277202628fcbce50e99
Andrei <student.andrey.2022@gmail.com>
2023-10-17 14:56:01 +0300

    updated route names

--- mycalendar/events/urls.py
+++ mycalendar/events/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import index, get_interval
 
 urlpatterns = [
-    path('', index),
-    path('interval/<int:interval_id>', get_interval),
+    path('', index, name='home'),
+    path('interval/<int:interval_id>', get_interval, name='interval'),
 ]
