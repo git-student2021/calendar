8c3131a4f55c35f9c864b80a7c35efc3aaa7e943
Andrei <student.andrey.2022@gmail.com>
2023-10-19 16:10:56 +0300

    Created list_interval.html. Updated _sidebar.html, events_tags.py, views.py

--- mycalendar/events/templatetags/events_tags.py
+++ mycalendar/events/templatetags/events_tags.py
@@ -7,3 +7,8 @@ register = template.Library()
 @register.simple_tag()
 def get_time_interval():
     return Time_interval.objects.all()
+
+@register.inclusion_tag('events/list_interval.html')
+def show_intervals():
+    intervals = Time_interval.objects.all()
+    return {"intervals": intervals}
--- mycalendar/events/views.py
+++ mycalendar/events/views.py
@@ -5,16 +5,13 @@ from .models import Events, Time_interval
 
 def index(request):
     events = Events.objects.all()
-    intervals = Time_interval.objects.all()
     context = {
         'events': events,
         'title': 'Список событий',
-        'intervals': intervals,
     }
     return render(request, 'events/index.html', context=context)
 
 def get_interval(request, interval_id):
     events = Events.objects.filter(interval_id=interval_id)
-    intervals = Time_interval.objects.all()
     interval = Time_interval.objects.get(pk=interval_id)
-    return render(request, 'events/interval.html', {'events': events, 'intervals': intervals, 'interval': interval})
+    return render(request, 'events/interval.html', {'events': events, 'interval': interval})
