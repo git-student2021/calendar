93a322086a032dadc0c1fc117a2af3876bc882e6
Andrei <student.andrey.2022@gmail.com>
2023-11-14 16:58:27 +0300

    Updated  forms.py (Working with forms. Part 2)

--- mycalendar/events/forms.py
+++ mycalendar/events/forms.py
@@ -3,8 +3,13 @@ from django import forms
 from .models import Time_interval
 
 class EventsForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    description_event = forms.CharField()
-    need_for_notification = forms.BooleanField()
-    interval = forms.ModelChoiceField(queryset=Time_interval.objects.all())
+    title = forms.CharField(max_length=150, label="Название события", widget=forms.TextInput(attrs={"class": "form-control"}))
+    description_event = forms.CharField(label="Описание", required=False, widget=forms.Textarea(
+        attrs={
+            "class": "form-control",
+            "rows": 5
+        }))
+    need_for_notification = forms.BooleanField(label="Напомнить?", initial=True)
+    interval = forms.ModelChoiceField(empty_label="Выберите время", label="Время за которое надо напомнить",
+    queryset=Time_interval.objects.all(), widget=forms.Select(attrs={"class": "form-control"}))
 
